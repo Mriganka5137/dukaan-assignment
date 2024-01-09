00fa21398466d995f5c934c0a83a99ea8e70b627
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-09 11:45:14 +0530

    Refactor TransactionPagination component

--- app/components/TransactionPagination.tsx
+++ app/components/TransactionPagination.tsx
@@ -1,67 +1,70 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useState } from "react";
 
 const TransactionPagination = () => {
   const [active, setActive] = useState(10);
 
   return (
-    <Pagination className=" mt-5 max-md:hidden">
-      <PaginationContent>
-        <PaginationItem
-          className=" border rounded-md mr-6"
-          onClick={() => {
-            if (active !== 1) setActive(active - 1);
-            else setActive(active);
-          }}
-        >
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem
-          onClick={() => setActive(1)}
-          className={`${
-            active === 1 && "bg-primary_blue text-black_100"
-          } rounded-md`}
-        >
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        {[10, 11, 12, 13, 14, 15, 16, 17, 18].map((item, index) => {
-          return (
-            <PaginationItem onClick={() => setActive(item)} key={index}>
-              <PaginationLink
-                href="#"
-                className={`${
-                  active === item && "bg-primary_blue text-black_100"
-                }`}
-              >
-                {item}
-              </PaginationLink>
-            </PaginationItem>
-          );
-        })}
+    <div className=" mt-5 max-md:hidden flex items-center gap-1.5 justify-center text-[14px] font-normal text-black_30 flex-wrap">
+      <Button
+        className=" border rounded-md mr-6 bg-black_100 flex items-center gap-1.5 "
+        disabled={active === 1}
+        onClick={() => {
+          setActive(active - 1);
+        }}
+      >
+        <Image
+          src="/icons/others/ChevronRight.svg"
+          width={18}
+          height={18}
+          alt="arrow"
+          className="mr-2"
+        />
+        Previous
+      </Button>
+      <Button
+        onClick={() => setActive(1)}
+        className={`${
+          active === 1 && "bg-primary_blue text-black_100 hover:bg-primary_blue"
+        } rounded-md `}
+      >
+        1
+      </Button>
+      <p>...</p>
+      {[10, 11, 12, 13, 14, 15, 16, 17, 18].map((item, index) => {
+        return (
+          <Button
+            onClick={() => setActive(item)}
+            key={index}
+            className={`${
+              active === item &&
+              "bg-primary_blue text-black_100 hover:bg-primary_blue "
+            }`}
+          >
+            {item}
+          </Button>
+        );
+      })}
 
-        <PaginationItem
-          className=" border rounded-md ml-6"
-          onClick={() => {
-            if (active !== 18) setActive(active + 1);
-            else setActive(active);
-          }}
-        >
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+      <Button
+        disabled={active === 18}
+        className=" border rounded-md ml-6 bg-black_100 flex items-center gap-1.5 "
+        onClick={() => {
+          setActive(active + 1);
+        }}
+      >
+        Next
+        <Image
+          src="/icons/others/ChevronRight.svg"
+          width={18}
+          height={18}
+          alt="arrow"
+          className="mr-2 rotate-180"
+        />
+      </Button>
+    </div>
   );
 };
 
